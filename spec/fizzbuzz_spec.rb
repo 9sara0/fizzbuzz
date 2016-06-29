0b08927a41f16dd779ad02c5048a6038bc85f6a6
Sara <sarah.alalaya@gmail.com>
2016-06-29 07:07:49 +0300

    Adding third test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,7 @@ describe 'fizzbuzz' do
   it 'return "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eql 'buzz'
   end
+  it 'return "fizzbuzz"  when passed 15' do
+    expect(fizzbuzz(15)).to eql 'fizzbuzz'
+  end
 end
